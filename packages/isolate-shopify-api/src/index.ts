fefc09d1c6570d8193e6b574cf000f7332bb2c2e
Long Nguyễn Giao Huỳnh <pierre@pierreneter.com>
2021-11-23 03:17:26 +0700

    chore: update isolate shopify

--- packages/isolate-shopify-api/src/index.ts
+++ packages/isolate-shopify-api/src/index.ts
@@ -6,24 +6,7 @@ const createIsolateShopify: (params: ContextParams) => ShopifyType = (
   params: ContextParams
 ) => {
   const shopify = Object.assign({}, Shopify);
-  const context = Object.assign({}, Shopify.Context);
-  const auth = Object.assign({}, Shopify.Auth);
-  const clients = Object.assign({}, Shopify.Clients);
-  const webhooks = Object.assign({}, Shopify.Webhooks);
-  const session = Object.assign({}, Shopify.Session);
-  const utils = Object.assign({}, Shopify.Utils);
-  const errors = Object.assign({}, Shopify.Errors);
-
-  context.initialize(params);
-
-  shopify.Context = context;
-  shopify.Auth = auth;
-  shopify.Clients = clients;
-  shopify.Webhooks = webhooks;
-  shopify.Session = session;
-  shopify.Errors = errors;
-  shopify.Utils = utils;
-
+  shopify.Context.initialize(params);
   return shopify;
 };
 
